fd572eb35e255e4924e0aceb66948b32b6f32af2
NiMaZi <zhengyl940425@gmail.com>
2020-07-30 17:39:17 +0200

    create scheduler log

--- etl/gs_utils.py
+++ etl/gs_utils.py
@@ -1,6 +1,17 @@
+import datetime
 from google.cloud import storage
 
 def check_new_file(bucket_name, prefix):
     client = storage.Client()
+    bucket = client.bucket(bucket_name)
+
+    curr_ts = datetime.datetime.now()
+    
+    blobs = list(bucket.list_blobs(prefix = prefix + 'scheduler_log'))
+
+    if not len(blobs):
+        blob = bucket_name.blob(prefix + 'scheduler_log')
+        blob.upload_from_string(curr_ts.strftime("%Y-%m-%d-%H-%M-%S"))
+
     for blob in client.list_blobs(bucket_name, prefix = prefix):
         print(str(blob))
--- etl/main.py
+++ etl/main.py
@@ -2,15 +2,14 @@ from bigquery_etl import *
 from dataflow_etl import *
 from gs_utils import *
 
-BUCKET_NAME = "dott_test"
-RAW_DATA_PREFIX = "/"
+BUCKET_NAME = "dott_de_assignment"
+RAW_DATA_PREFIX = "raw_data_landing/"
 
 def etl_core():
     pass
 
 def main():
     check_new_file(BUCKET_NAME, RAW_DATA_PREFIX)
-    pass
 
 if __name__ == '__main__':
     main()
