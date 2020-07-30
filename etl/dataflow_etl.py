7965584e38eb5b607c87bd711e03f3bc61485364
NiMaZi <zhengyl940425@gmail.com>
2020-07-30 19:30:38 +0200

    fix

--- etl/dataflow_etl.py
+++ etl/dataflow_etl.py
@@ -9,17 +9,17 @@ def dataflow_pipeline_run():
         rides = p | 'ReadRides' >> beam.io.ReadFromText('gs://dott_test/rides*.csv')
 
         
-        picks | 'PickupsCountBeforeDedup' >> beam.combiners.Count.Globally() | 'PickupsNameIt1' >> beam.Map(lambda x: f"{x} pickup records before deduplication.") | 'PickupsPrintCount1' >> beam.Map(logging.error)
-        depls | 'DeploymentsCountBeforeDedup' >> beam.combiners.Count.Globally() | 'DeploymentsNameIt1' >> beam.Map(lambda x: f"{x} deployment records before deduplication.") | 'DeploymentsPrintCount1' >> beam.Map(logging.error)
-        rides | 'RidesCountBeforeDedup' >> beam.combiners.Count.Globally() | 'RidesNameIt1' >> beam.Map(lambda x: f"{x} ride records before deduplication.") | 'RidesPrintCount1' >> beam.Map(logging.error)
+        picks | 'PickupsCountBeforeDedup' >> beam.combiners.Count.Globally() | 'PickupsNameIt1' >> beam.Map(lambda x: (x, 'PickupsBeforeDedup')) | 'PickupsPrintCount1' >> beam.Map(logging.error)
+        depls | 'DeploymentsCountBeforeDedup' >> beam.combiners.Count.Globally() | 'DeploymentsNameIt1' >> beam.Map(lambda x: (x, 'DeploymentsBeforeDedup')) | 'DeploymentsPrintCount1' >> beam.Map(logging.error)
+        rides | 'RidesCountBeforeDedup' >> beam.combiners.Count.Globally() | 'RidesNameIt1' >> beam.Map(lambda x: (x, 'RidesBeforeDedup')) | 'RidesPrintCount1' >> beam.Map(logging.error)
 
         picks_dedup = picks | 'DeDupPickups' >> beam.Distinct()
         depls_dedup = depls | 'DeDupDeployments' >> beam.Distinct()
         rides_dedup = rides | 'DeDupRides' >> beam.Distinct()
 
-        picks_dedup | 'PickupsCountAfterDedup' >> beam.combiners.Count.Globally() | 'PickupsNameIt2' >> beam.Map(lambda x: f"{x} pickup records after deduplication.") | 'PickupsPrintCount2' >> beam.Map(logging.error)
-        picks_dedup | 'DeploymentsCountAfterDedup' >> beam.combiners.Count.Globally() | 'DeploymentsNameIt2' >> beam.Map(lambda x: f"{x} deployment records after deduplication.") | 'DeploymentsPrintCount2' >> beam.Map(logging.error)
-        picks_dedup | 'RidesCountAfterDedup' >> beam.combiners.Count.Globally() | 'RidesNameIt2' >> beam.Map(lambda x: f"{x} ride records after deduplication.") | 'RidesPrintCount2' >> beam.Map(logging.error)
+        picks_dedup | 'PickupsCountAfterDedup' >> beam.combiners.Count.Globally() | 'PickupsNameIt2' >> beam.Map(lambda x: (x, 'PickupsAfterDedup')) | 'PickupsPrintCount2' >> beam.Map(logging.error)
+        picks_dedup | 'DeploymentsCountAfterDedup' >> beam.combiners.Count.Globally() | 'DeploymentsNameIt2' >> beam.Map(lambda x: (x, 'DeploymentsAfterDedup')) | 'DeploymentsPrintCount2' >> beam.Map(logging.error)
+        picks_dedup | 'RidesCountAfterDedup' >> beam.combiners.Count.Globally() | 'RidesNameIt2' >> beam.Map(lambda x: (x, 'RidesAfterDedup')) | 'RidesPrintCount2' >> beam.Map(logging.error)
 
         picks_dedup | 'WritePickups' >> beam.io.WriteToText('gs://dott_test/pickups_final.csv', num_shards = 1, shard_name_template = "")
         depls_dedup | 'WriteDeployments' >> beam.io.WriteToText('gs://dott_test/deployments_final.csv', num_shards = 1, shard_name_template = "")
