3940dbfc3475399c4808051591475a7902c41e5c
NiMaZi <zhengyl940425@gmail.com>
2020-07-30 19:27:37 +0200

    log all dedups

--- etl/dataflow_etl.py
+++ etl/dataflow_etl.py
@@ -8,43 +8,22 @@ def dataflow_pipeline_run():
         depls = p | 'ReadDeployments' >> beam.io.ReadFromText('gs://dott_test/deployments*.csv')
         rides = p | 'ReadRides' >> beam.io.ReadFromText('gs://dott_test/rides*.csv')
 
-        (
-            picks 
-            | 'CountBeforeDedup' >> beam.combiners.Count.Globally()
-            | 'NameIt1' >> beam.Map(lambda x: (x, 'BeforeDedup'))
-            | 'PrintCount1' >> beam.Map(logging.error)
-        )
+        
+        picks | 'PickupsCountBeforeDedup' >> beam.combiners.Count.Globally() | 'PickupsNameIt1' >> beam.Map(lambda x: f"{x} pickup records before deduplication.") | 'PickupsPrintCount1' >> beam.Map(logging.error)
+        depls | 'DeploymentsCountBeforeDedup' >> beam.combiners.Count.Globally() | 'DeploymentsNameIt1' >> beam.Map(lambda x: f"{x} deployment records before deduplication.") | 'DeploymentsPrintCount1' >> beam.Map(logging.error)
+        rides | 'RidesCountBeforeDedup' >> beam.combiners.Count.Globally() | 'RidesNameIt1' >> beam.Map(lambda x: f"{x} ride records before deduplication.") | 'RidesPrintCount1' >> beam.Map(logging.error)
 
         picks_dedup = picks | 'DeDupPickups' >> beam.Distinct()
         depls_dedup = depls | 'DeDupDeployments' >> beam.Distinct()
         rides_dedup = rides | 'DeDupRides' >> beam.Distinct()
 
-        (
-            picks_dedup 
-            | 'CountAfterDedup' >> beam.combiners.Count.Globally()
-            | 'NameIt2' >> beam.Map(lambda x: (x, 'AfterDedup'))
-            | 'PrintCount2' >> beam.Map(logging.error)
-        )
+        picks_dedup | 'PickupsCountAfterDedup' >> beam.combiners.Count.Globally() | 'PickupsNameIt2' >> beam.Map(lambda x: f"{x} pickup records after deduplication.") | 'PickupsPrintCount2' >> beam.Map(logging.error)
+        picks_dedup | 'DeploymentsCountAfterDedup' >> beam.combiners.Count.Globally() | 'DeploymentsNameIt2' >> beam.Map(lambda x: f"{x} deployment records after deduplication.") | 'DeploymentsPrintCount2' >> beam.Map(logging.error)
+        picks_dedup | 'RidesCountAfterDedup' >> beam.combiners.Count.Globally() | 'RidesNameIt2' >> beam.Map(lambda x: f"{x} ride records after deduplication.") | 'RidesPrintCount2' >> beam.Map(logging.error)
 
         picks_dedup | 'WritePickups' >> beam.io.WriteToText('gs://dott_test/pickups_final.csv', num_shards = 1, shard_name_template = "")
         depls_dedup | 'WriteDeployments' >> beam.io.WriteToText('gs://dott_test/deployments_final.csv', num_shards = 1, shard_name_template = "")
         rides_dedup | 'WriteRides' >> beam.io.WriteToText('gs://dott_test/rides_final.csv', num_shards = 1, shard_name_template = "")
-
-        # table_schema = {
-        #     'fields': [
-        #         {'name': 'task_id', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        #         {'name': 'vehicle_id', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #         {'name': 'qr_code', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #         {'name': 'time_task_created', 'type': 'TIMESTAMP', 'mode': 'NULLABLE'},
-        #         {'name': 'time_task_resolved', 'type': 'TIMESTAMP', 'mode': 'NULLABLE'},
-        #     ]
-        # }
-
-        # picks_dedup | beam.io.gcp.bigquery.WriteToBigQuery(
-        #     'peaceful-tide-284813:dott_test.pickups',
-        #     schema = table_schema
-        # )
-
 
 if __name__ == '__main__':
     dataflow_pipeline_run()
