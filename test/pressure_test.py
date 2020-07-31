229a9dec45554f598b05ac1cfaf7b42cf95e63dd
NiMaZi <zhengyl940425@gmail.com>
2020-07-31 14:46:55 +0200

    test and requirements

---
+++ test/pressure_test.py
@@ -0,0 +1,25 @@
+import sys
+import random
+import grequests
+from time import time
+
+QR_CODE_LIST = []
+URL_PREFIX = "http://peaceful-tide-284813.ew.r.appspot.com/vehicles/"
+
+def run(num_requests = 5000):
+    sample_qr_codes = random.choices(QR_CODE_LIST, k = num_requests)
+    urls = [URL_PREFIX + qr_code for qr_code in sample_qr_codes]
+
+    rs = (grequests.get(u) for u in urls)
+    s = time()
+    res = grequests.map(rs)
+    e = time()
+    valid_res = [r for r in res if r]
+
+    print("Got {} valid responses in {} seconds.")
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        run(int(sys.argv[1]))
+    else:
+        run()
