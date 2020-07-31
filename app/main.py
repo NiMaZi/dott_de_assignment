dab1572927125d9ea6022f4a1ba44edf1fad5fc0
NiMaZi <zhengyl940425@gmail.com>
2020-07-31 19:05:02 +0200

    fix cache

--- app/main.py
+++ app/main.py
@@ -9,7 +9,13 @@ except:
 
 PROJECT = 'peaceful-tide-284813'
 
+config = {
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 60
+}
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 @app.route('/vehicles/<key>', methods=['GET'])
 @cache.cached(timeout = 60)
