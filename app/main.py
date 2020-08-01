eb2b3b635ebda6c435a8ad021c8ebfaacda8c330
NiMaZi <zhengyl940425@gmail.com>
2020-08-01 12:10:22 +0200

    update redis

--- app/main.py
+++ app/main.py
@@ -12,9 +12,9 @@ PROJECT = 'peaceful-tide-284813'
 
 app = Flask(__name__)
 
-redis_host = os.environ.get('REDISHOST')
-redis_port = int(os.environ.get('REDISPORT'))
-redis_client = redis.StrictRedis(host = redis_host, port = redis_port)
+# redis_host = os.environ.get('REDISHOST')
+# redis_port = int(os.environ.get('REDISPORT'))
+redis_client = redis.StrictRedis(host = "10.166.182.235", port = 6379)
 bq_client = bigquery.Client(project = PROJECT)
 
 @app.route('/vehicles/<key>', methods = ['GET'])
