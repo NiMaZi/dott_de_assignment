5da9bfdfb0279e2a634dcccf11d666a71bd76d9e
NiMaZi <zhengyl940425@gmail.com>
2020-08-02 14:34:50 +0200

    Update pressure_test.py

--- test/pressure_test.py
+++ test/pressure_test.py
@@ -13,4 +13,4 @@ class MyUser(HttpUser):
 
 if __name__ == "__main__":
     import os
-    os.system("locust -f pressure_test.py --host http://peaceful-tide-284813.ew.r.appspot.com --headless -u 1000 -r 50 -t 5m --csv pressure_test") 
+    os.system("locust -f pressure_test.py --host http://dott-de-assignment.ew.r.appspot.com --headless -u 1000 -r 50 -t 5m --csv pressure_test") 
